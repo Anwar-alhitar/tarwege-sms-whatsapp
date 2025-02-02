cffa993352bf185f4ac3c38d97368b21a0da56cb
Anwar_alhitar <anwaralhitar@gmail.com>
2025-02-03 01:11:54 +0300

    create System service

---
+++ src/Services/SystemService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tarwege\SmsWhatsapp\Services;
+
+class SystemService
+{
+    protected $client;
+
+    public function __construct(TarwegeClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Get gateway rates.
+     */
+    public function getGatewayRates(array $params = [])
+    {
+        return $this->client->callApi('/system/gateway-rates', 'GET', $params);
+    }
+
+    /**
+     * Get shorteners.
+     */
+    public function getShorteners(array $params = [])
+    {
+        return $this->client->callApi('/system/shorteners', 'GET', $params);
+    }
+}
