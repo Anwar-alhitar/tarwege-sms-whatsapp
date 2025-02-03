bb941c3852c45787e314e8461816b4bec5827a21
Anwar_alhitar <anwaralhitar@gmail.com>
2025-02-04 00:40:40 +0300

    return type

--- src/Facades/Tarwege.php
+++ src/Facades/Tarwege.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Facade;
 
 class Tarwege extends Facade
 {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return \Tarwege\SmsWhatsapp\Services\TarwegeClient::class;
